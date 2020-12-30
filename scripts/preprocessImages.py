c9cdefc0c228e239a46d8c538b2a729e511ce83b
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2020-12-30 14:08:29 +0100

    and resizing images script

---
+++ scripts/preprocessImages.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+
+for filename in os.listdir('/Users/lucapomer/Documents/bachelor/YogaPoseDetection/downwardDogSmall'):
+    img = cv2.imread(os.path.join('/Users/lucapomer/Documents/bachelor/YogaPoseDetection/downwardDogSmall', filename))
+    print('Original Dimensions : ', img.shape)
+    cv2.imwrite('/Users/lucapomer/Documents/bachelor/YogaPoseDetection/resizedImages/' + str(filename) + '.jpg', cv2.resize(img, (256, 256), interpolation=cv2.INTER_AREA))
+
+# scale_percent = 60  # percent of original size
+# width = int(img.shape[1] * scale_percent / 100)
+# height = int(img.shape[0] * scale_percent / 100)
+# dim = (width, height)
+# # resize image
+# resized = cv2.resize(img, (256, 256), interpolation=cv2.INTER_AREA)
+#
+# print('Resized Dimensions : ', resized.shape)
+#
+# cv2.imshow("Resized image", resized)
+# cv2.waitKey(0)
+cv2.destroyAllWindows()
