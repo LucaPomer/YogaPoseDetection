7351ab723ff0bae9d80eaca11b63106cf5c9d2f3
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2021-01-13 16:23:44 +0100

    added function for splitting the image files

--- scripts/main.py
+++ scripts/main.py
@@ -5,16 +5,18 @@ import cv2
 from scripts.helpers.data_manipulation_helpers import get_keypoint_angles, write_data
 from scripts.helpers.openpose_accuraccy_testing import get_accuracy, optimal_net_res
 from scripts.openposeKeypoints import get_openpose_keypoints
+from scripts.preprocessImages import split_file
 
 class_number = 3
-input_file_path = '/Users/lucapomer/Documents/bachelor/YogaPoseDetection/downwardDogSmall'
+input_file_path = '/Users/lucapomer/Documents/bachelor/YogaPoseDetection/downwarddog'
 result_file_path = '/Users/lucapomer/Documents/bachelor/YogaPoseDetection/angles.csv'
 
+split_file(input_file_path, 15)
 
 net_res_width = -1
 net_res_height = 368
 
-optimal_net_res(input_file_path, 8, 128, 64)
+# optimal_net_res(input_file_path, 8, 128, 64)
 
 # result_from_openpose = get_openpose_keypoints(net_res_width, net_res_height, input_file_path)
 
--- scripts/preprocessImages.py
+++ scripts/preprocessImages.py
@@ -1,10 +1,31 @@
 import cv2
 import os
+import shutil
 
-for filename in os.listdir('/experiments/accuraccyTest'):
-    img = cv2.imread(os.path.join('/experiments/accuraccyTest', filename))
-    print('Original Dimensions : ', img.shape)
-    cv2.imwrite('/Users/lucapomer/Documents/bachelor/YogaPoseDetection/croppedAndResized/' + str(filename) + '.jpg', cv2.resize(img, (256, 256), interpolation=cv2.INTER_AREA))
+
+# resizing images
+# for filename in os.listdir('/experiments/accuraccyTest'): img = cv2.imread(os.path.join(
+# '/experiments/accuraccyTest', filename)) print('Original Dimensions : ', img.shape) cv2.imwrite(
+# '/Users/lucapomer/Documents/bachelor/YogaPoseDetection/croppedAndResized/' + str(filename) + '.jpg', cv2.resize(
+# img, (256, 256), interpolation=cv2.INTER_AREA))
+#
+
+def split_file(file_path, img_per_folder):
+    batch_num = 1
+    dst_path = file_path + '/batch' + str(batch_num)
+    os.makedirs(dst_path)
+    num_images_in_batch = 0
+    for img_path in os.listdir(file_path):
+        print(img_path)
+        full_img_path = file_path + '/' + img_path
+        if num_images_in_batch >= img_per_folder:
+            batch_num += 1
+            dst_path = file_path + '/batch' + str(batch_num)
+            os.makedirs(dst_path)
+            num_images_in_batch = 0
+        shutil.move(full_img_path, dst_path)
+        num_images_in_batch += 1
+
 
 # scale_percent = 60  # percent of original size
 # width = int(img.shape[1] * scale_percent / 100)
