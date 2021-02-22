b2c2bffb874c899e5c2f7b8132d606d28b72e350
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2021-02-22 15:46:01 +0100

    fixed data visualisation

--- scripts/data_visualisation.py
+++ scripts/data_visualisation.py
@@ -23,38 +23,47 @@ from sklearn.preprocessing import StandardScaler
 from scripts.ml_data_for_classification import MlDataForModelTraining
 
 all_data = MlDataForModelTraining('/Users/lucapomer/Documents/bachelor/YogaPoseDetection/csv_data_files/angles_with_flipped.csv', 0.33, 42)
-# my_data = genfromtxt('/Users/lucapomer/Documents/bachelor/YogaPoseDetection/anglesNew.csv', delimiter=',',
-#                      usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-# classes = genfromtxt('/Users/lucapomer/Documents/bachelor/YogaPoseDetection/anglesNew.csv', delimiter=',', usecols=(11,))
+
 
 #  OPTION 1: show some of the data for example some specific angles
-# plt.scatter(my_data[classes == 1][0], my_data[classes == 1][1], label='class 1', c='red')
-# plt.scatter(my_data[classes == 3][0], my_data[classes == 3][1], label='class 3', c='blue')
+# plt.scatter(all_data.data[all_data.class_labels == 1][0], all_data.data[all_data.class_labels == 1][1], label='tree', c='red')
+# plt.scatter(all_data.data[all_data.class_labels == 3][0], all_data.data[all_data.class_labels == 3][1], label='bridge', c='blue')
 #
 # plt.show()
 
 #  OPTION 2: use Dimensionality Reduction with Principle Component Analysis (PCA)
 # pca = sklearn_pca(n_components=2)  # 2-dimensional PCA
-# transformed = pd.DataFrame(pca.fit_transform(my_data))
-#
-# plt.scatter(transformed[classes == 1][0], transformed[classes == 1][1], label='class 1', c='red')
-# plt.scatter(transformed[classes == 3][0], transformed[classes == 3][1], label='class 3', c='blue')
-# plt.scatter(transformed[classes == 2][0], transformed[classes == 2][1], label='class 2', c='green')
-#
+# transformed = pd.DataFrame(pca.fit_transform(all_data.data, all_data.class_labels))
+# #
+# plt.scatter(transformed[all_data.class_labels == 1][0], transformed[all_data.class_labels == 1][1], label='tree' )
+# plt.scatter(transformed[all_data.class_labels == 2][0], transformed[all_data.class_labels == 2][1], label='downwarddog' )
+# plt.scatter(transformed[all_data.class_labels == 3][0], transformed[all_data.class_labels == 3][1], label='bridge')
+# plt.scatter(transformed[all_data.class_labels == 4][0], transformed[all_data.class_labels == 4][1], label='child')
+# plt.scatter(transformed[all_data.class_labels == 5][0], transformed[all_data.class_labels == 5][1], label='mountain')
+# plt.scatter(transformed[all_data.class_labels == 6][0], transformed[all_data.class_labels == 6][1], label='plank')
+# plt.scatter(transformed[all_data.class_labels == 7][0], transformed[all_data.class_labels == 7][1], label='seatedforward')
+# plt.scatter(transformed[all_data.class_labels == 8][0], transformed[all_data.class_labels == 8][1], label='triangle')
+# plt.scatter(transformed[all_data.class_labels == 9][0], transformed[all_data.class_labels == 9][1], label='warrior1')
+# plt.scatter(transformed[all_data.class_labels == 10][0], transformed[all_data.class_labels == 10][1], label='warrior2')
+# #
 # plt.legend()
 # plt.show()
 
 #  OPTION 3: use Dimensionality Reduction with Linear Discriminant Analysis
-lda = make_pipeline(StandardScaler(),
-                    LinearDiscriminantAnalysis(n_components=2))
-transformed = pd.DataFrame(lda.fit_transform(all_data.data, all_data.class_labels))
-
-plt.scatter(transformed[all_data.class_labels == 1][0], transformed[all_data.class_labels == 1][1], label='tree', c='red')
-plt.scatter(transformed[all_data.class_labels == 3][0], transformed[all_data.class_labels == 3][1], label='bridge', c='blue')
-plt.scatter(transformed[all_data.class_labels == 2][0], transformed[all_data.class_labels == 2][1], label='downwardDog', c='green')
-plt.scatter(transformed[all_data.class_labels == 4][0], transformed[all_data.class_labels == 4][1], label='tree', c='purple')
-plt.scatter(transformed[all_data.class_labels == 5][0], transformed[all_data.class_labels == 5][1], label='bridge', c='black')
-plt.scatter(transformed[all_data.class_labels == 6][0], transformed[all_data.class_labels == 6][1], label='downwardDog', c='orange')
-
-plt.legend(loc=2)
-plt.show()
+# lda = make_pipeline(StandardScaler(),
+#                     LinearDiscriminantAnalysis(n_components=2))
+# transformed = pd.DataFrame(lda.fit_transform(all_data.data, all_data.class_labels))
+#
+# plt.scatter(transformed[all_data.class_labels == 1][0], transformed[all_data.class_labels == 1][1], label='tree' )
+# plt.scatter(transformed[all_data.class_labels == 2][0], transformed[all_data.class_labels == 2][1], label='downwarddog' )
+# plt.scatter(transformed[all_data.class_labels == 3][0], transformed[all_data.class_labels == 3][1], label='bridge')
+# plt.scatter(transformed[all_data.class_labels == 4][0], transformed[all_data.class_labels == 4][1], label='child')
+# plt.scatter(transformed[all_data.class_labels == 5][0], transformed[all_data.class_labels == 5][1], label='mountain')
+# plt.scatter(transformed[all_data.class_labels == 6][0], transformed[all_data.class_labels == 6][1], label='plank')
+# plt.scatter(transformed[all_data.class_labels == 7][0], transformed[all_data.class_labels == 7][1], label='seatedforward')
+# plt.scatter(transformed[all_data.class_labels == 8][0], transformed[all_data.class_labels == 8][1], label='triangle')
+# plt.scatter(transformed[all_data.class_labels == 9][0], transformed[all_data.class_labels == 9][1], label='warrior1')
+# plt.scatter(transformed[all_data.class_labels == 10][0], transformed[all_data.class_labels == 10][1], label='warrior2')
+#
+# plt.legend(loc=2)
+# plt.show()
